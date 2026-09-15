fa72768e6c84a380891f5d4497f6952fbf1c577a
meneurwz-glitch <meneurwz@gmail.com>
2026-09-15 16:35:13 +0800

    Update video_card_v.dart

--- lib/common/widgets/video_card/video_card_v.dart
+++ lib/common/widgets/video_card/video_card_v.dart
@@ -1,4 +1,5 @@
 import 'package:PiliPlus/common/style.dart';
+import 'package:PiliPlus/utils/utils.dart';
 import 'package:PiliPlus/common/widgets/badge.dart';
 import 'package:PiliPlus/common/widgets/image/image_save.dart';
 import 'package:PiliPlus/common/widgets/image/network_img_layer.dart';
@@ -109,11 +110,16 @@ class VideoCardV extends StatelessWidget {
                       return Stack(
                         clipBehavior: Clip.none,
                         children: [
-                          NetworkImgLayer(
-                            src: videoItem.cover,
-                            width: maxWidth,
-                            height: maxHeight,
-                            borderRadius: const .vertical(top: .circular(12)),
+                            Hero(
+                            tag: Utils.makeHeroTag(
+                              videoItem.bvid ?? videoItem.aid,
+                            ),
+                            child: NetworkImgLayer(
+                              src: videoItem.cover,
+                              width: maxWidth,
+                              height: maxHeight,
+                              borderRadius: const .vertical(top: .circular(12)),
+                            ),
                           ),
                           if (videoItem.duration > 0)
                             PBadge(
