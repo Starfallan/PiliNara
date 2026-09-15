ab3766a43ff1374844e61a19e7332fbed7fb7f86
meneurwz-glitch <meneurwz@gmail.com>
2026-09-15 11:25:34 +0800

    Update storage_key.dart

--- lib/utils/storage_key.dart
+++ lib/utils/storage_key.dart
@@ -305,6 +305,7 @@ abstract final class SettingBoxKey {
       autoSideBar = 'autoSideBar',
       sideBarThreshold = 'sideBarThreshold',
       enableMYBar = 'enableMYBar',
+      enableGradientBg = 'enableGradientBg',
       hideTopBar = 'hideSearchBar',
       hideBottomBar = 'hideTabBar',
       barHideType = 'barHideType',
