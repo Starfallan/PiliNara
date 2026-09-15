dbf894787f76f8121e5635e6b58bb13bad6a09e3
meneurwz-glitch <meneurwz@gmail.com>
2026-09-15 15:03:44 +0800

    Update view.dart

--- lib/pages/main/view.dart
+++ lib/pages/main/view.dart
@@ -552,11 +552,17 @@ class _MainAppState extends PopScopeState<MainApp>
                 height: MediaQuery.sizeOf(context).height,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
-                    colors: [
-                      Theme.of(context).colorScheme.primary.withValues(alpha: 0.6),
-                      Theme.of(context).colorScheme.primaryContainer.withValues(alpha: 0.6),
-                      Theme.of(context).colorScheme.surface,
-                    ],
+                    colors: _colorScheme.brightness == Brightness.light
+                        ? [
+                            _colorScheme.primaryContainer.withValues(alpha: 0.6),
+                            _colorScheme.surfaceContainerHighest.withValues(alpha: 0.6),
+                            _colorScheme.surface,
+                          ]
+                        : [
+                            _colorScheme.primary.withValues(alpha: 0.6),
+                            _colorScheme.primaryContainer.withValues(alpha: 0.6),
+                            _colorScheme.surface,
+                          ],
                     begin: Alignment.topLeft,
                     end: Alignment.bottomRight,
                     stops: const [0.1, 0.4, 0.7],
