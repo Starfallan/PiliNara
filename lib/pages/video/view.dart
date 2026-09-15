9917df043f4e77d0bdcfd7ea9e90e4c4ad0eca5d
meneurwz-glitch <meneurwz@gmail.com>
2026-09-15 17:17:49 +0800

    Update view.dart

--- lib/pages/video/view.dart
+++ lib/pages/video/view.dart
@@ -2207,7 +2207,7 @@ class _VideoDetailPageVState extends State<VideoDetailPageV>
           ),
         ),
 
-        Hero(tag: heroTag, child: plPlayer(width: width, height: height)),
+         plPlayer(width: width, height: height),
 
         Obx(() {
           if (!videoDetailController.autoPlay) {
